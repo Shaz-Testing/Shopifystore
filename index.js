48bcd0c0dbe1f080b5257500a427e0a13573a3f0
Shaz-Testing <javedshuja112@gmail.com>
2025-04-12 15:48:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -69,6 +69,6 @@ app.post("/update-stock", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 1000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Listening on port ${PORT}`)); // ✅
 
